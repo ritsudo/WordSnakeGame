e2b65cb747d48b1b62b14dbcaa15c5bdb3826908
ritsudo <minato-namikaje@yandex.ru>
2024-04-22 21:35:48 +0300

    feat: add letters

--- nodejs-server/public/game.js
+++ nodejs-server/public/game.js
@@ -1,17 +1,22 @@
     const canvas = document.getElementById("gameCanvas");
     const ctx = canvas.getContext("2d");
     const box = 20;
-    let snake = [{ x: 10, y: 10 }];
+    let snake = [];
     let dx = 0;
     let dy = 0;
     let food = { x: 15, y: 15 };
 	
-	let words = ['АВТОМОБИЛЬ', 'КОШКА', 'ЧАШКА', 'ДЕРЕВО', 'СОЛНЦЕ', 'КНИГА', 'ЧАСЫ', 'КВАРТИРА', 'ЯБЛОКО', 'БУТЫЛКА'];
+	let randomNumber;
+	
+	let words = ['АВТОМОБИЛЬ', 'КОШКА', 'ЧАШКА', 'ДЕРЕВО', 'СОЛНЦЕ', 'КНИГА', 'ЧАСЫ', 'ДОМ', 'ЯБЛОКО', 'БУТЫЛКА'];
 
     function drawSnake() {
       snake.forEach(segment => {
-        ctx.fillStyle = "green";
+        ctx.fillStyle = "white";
         ctx.fillRect(segment.x * box, segment.y * box, box, box);
+		ctx.font = '20px Arial';
+		ctx.fillStyle = 'black';
+		ctx.fillText(segment.letter, segment.x*box, segment.y*box + (box - 2));
         ctx.strokeStyle = "darkgreen";
         ctx.strokeRect(segment.x * box, segment.y * box, box, box);
       });
@@ -21,13 +26,20 @@
       ctx.fillStyle = "red";
       ctx.fillRect(food.x * box, food.y * box, box, box);
     }
+	
+	function fillSnake() {
+		snake.splice(0, snake.length);
+		randomNumber = Math.floor(Math.random() * 10);
+		let randomElementLength = words[randomNumber].length;
+		for (let i = 0; i < randomElementLength; i+=1) {
+			snake.push({x: 10 + i, y: 10, letter: words[randomNumber][i]});
+		}
+	}
 
     function moveSnake() {
 		ddx = food.x - snake[0].x;
 		ddy = food.y - snake[0].y;
-		
-//		console.log(ddx, ddy);
-		
+			
 		if (ddx != 0) {
 			if (dy != 0) {
 				dy = 0
@@ -49,10 +61,16 @@
 			}
 		}
 		
-      const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+		//
+      const head = { x: snake[0].x + dx, y: snake[0].y + dy, letter: words[randomNumber][0] };
+	  for (let q = 0; q < words[randomNumber].length; q++) {
+		  snake[q].letter = words[randomNumber][q+1]
+	  }
       snake.unshift(head);
+	  //
       if (head.x === food.x && head.y === food.y) {
         generateFood();
+		fillSnake();
       } else {
         snake.pop();
       }
@@ -93,21 +111,7 @@
 
     document.addEventListener("keydown", event => {
       const direction = event.key.replace("Arrow", "");
-	  /*
-      if (direction === "Up" && dy === 0) {
-        dx = 0;
-        dy = -1;
-      } else if (direction === "Down" && dy === 0) {
-        dx = 0;
-        dy = 1;
-      } else if (direction === "Left" && dx === 0) {
-        dx = -1;
-        dy = 0;
-      } else if (direction === "Right" && dx === 0) {
-        dx = 1;
-        dy = 0;
-      }
-	  */
+
 	  if (direction === "Up") {
         food.y -= 1;
       } else if (direction === "Down") {
@@ -120,4 +124,5 @@
     });
 
     generateFood();
-    const game = setInterval(main, 100);
+	fillSnake();
+    const game = setInterval(main, 140);
